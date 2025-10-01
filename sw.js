207a0b3ef4f4273f96c07a44e223344c931f3dd2
anjbdj <anjbdj1@gmail.com>
2025-10-01 15:36:44 +0530

    HTML site to basic PWA!

--- sw.js
+++ sw.js
@@ -1,54 +1,10 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
-const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
-// Install event - caching files
-self.addEventListener('install', (event) => {
-  console.log('[ServiceWorker] Install');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting(); // Activate worker immediately
-});
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
 
-// Activate event - clean up old caches
-self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim(); // Take control of pages
-});
+  workbox.precaching.precacheAndRoute([]);
 
-// Fetch event - respond with cached resources or fetch from network
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).catch(() =>
-          caches.match('/offline.html') // Optional: show offline page
-        )
-      );
-    })
-  );
-});
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
