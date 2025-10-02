9a27dc67e569e98d4ed1fa1d0a9786ec741deb8c
anjbdj <anjbdj1@gmail.com>
2025-10-02 09:58:07 +0530

    HTML site to basic PWA!

--- sw.js
+++ sw.js
@@ -1,5 +1,19 @@
-self.addEventListener("install", (e) => {
-console.log("Srvcwrkr");
+var staticCacheName = "pwa";
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(["/"]);
+    })
+  );
 });
 
+self.addEventListener("fetch", function (event) {
+  console.log(event.request.url);
 
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
