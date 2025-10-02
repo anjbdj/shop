3d500d80c307992ff96531807c01034a760cb93d
anjbdj <anjbdj1@gmail.com>
2025-10-02 10:09:13 +0530

    HTML site to basic PWA!

--- sw.js
+++ sw.js
@@ -1,16 +1,23 @@
-var staticCacheName = "pwa";
+var staticCacheName = "pwa-v1";
 
+// Install Event: Cache assets
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/app.js",
+        "/icons/icon-192.png",
+        "icons/icon-512.png"
+      ]);
     })
   );
 });
 
+// Fetch Event: Serve from Cache or Network
 self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
-
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
