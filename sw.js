1740376eaca1a36df6e6b3a46c3c28f2b7c39a61
anjbdj <anjbdj1@gmail.com>
2025-10-02 09:13:52 +0530

    HTML site to basic PWA!

--- sw.js
+++ sw.js
@@ -1,36 +1,5 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
-
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-
-  workbox.precaching.precacheAndRoute([]);
-
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-} 
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the default prompt from showing
-  e.preventDefault();
-  // Save the event for later use
-  deferredPrompt = e;
-
-
-
-  self.addEventListener('click', () => {
-    // Show the prompt
-    deferredPrompt.prompt();
-    
-    // Wait for the user's response to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      deferredPrompt = null;
-    });
-  });
+self.addEventListener("install", (e) => {
+console.log("Srvcwrkr");
 });
+
+
